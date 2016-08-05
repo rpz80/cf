63fd87dd58879144f1e4058ac1b6f51ab61a2ceb
rpz80 <rippa80@gmail.com>
2016-08-05 19:12:28 +0300

    more todos

--- cf/time_watcher.h
+++ cf/time_watcher.h
@@ -9,6 +9,9 @@
 #include <thread>
 #include <cf/common.h>
 
+// TODO: rework timeout start points.
+// timeout should start together with continuation.
+
 namespace cf {
 class time_watcher {
   struct record {
