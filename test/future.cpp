df5d7d4b188c5bddf8d2236cb6f5072d997e379f
rpz80 <rippa80@gmail.com>
2016-08-05 19:02:47 +0300

    more time watcher tests

--- test/future.cpp
+++ test/future.cpp
@@ -433,13 +433,113 @@ TEST_CASE("Time watcher. Future timeout") {
 
       } catch (const connect_timeout& e) {
         REQUIRE(e.what() == std::string("Connect timeout"));
-        std::cerr << e.what() << std::endl;
       } catch (const write_timeout& e) {
         REQUIRE(false);
         std::cerr << e.what() << std::endl;
       } catch (const read_timeout& e) {
         REQUIRE(false);
         std::cerr << e.what() << std::endl; 
+      }
+    }
+    
+    SECTION("write timeout") {
+      tcp_client client(std::chrono::milliseconds(100),
+                        std::chrono::milliseconds(400),
+                        std::chrono::milliseconds(200));
+      try {
+        auto client_future = cf::async([client = std::move(client)] () mutable {
+          client.connect("mysite.com:8001");
+          return std::move(client);
+        }).timeout(std::chrono::milliseconds(500), connect_timeout("Connect timeout"), tw).then(executor,
+        [](cf::future<tcp_client> client_future) mutable {
+          auto client = client_future.get();
+          client.write("GET /");
+          return client;
+        }).timeout(std::chrono::milliseconds(100), write_timeout("Write timeout"), tw).then(executor,
+        [](cf::future<tcp_client> client_future) mutable {
+          auto client = client_future.get();
+          client.read_until("/r/n/r/n");
+          return client;
+        }).timeout(std::chrono::seconds(2), read_timeout("Read timeout"), tw);
+        
+        std::cout << client_future.get().data() << std::endl;
+        REQUIRE(false);
+
+      } catch (const connect_timeout& e) {
+        REQUIRE(false);
+        std::cerr << e.what() << std::endl;
+      } catch (const write_timeout& e) {
+        REQUIRE(e.what() == std::string("Write timeout"));
+      } catch (const read_timeout& e) {
+        REQUIRE(false);
+        std::cerr << e.what() << std::endl; 
+      }
+    }
+    
+    SECTION("read timeout") {
+      tcp_client client(std::chrono::milliseconds(100),
+                        std::chrono::milliseconds(100),
+                        std::chrono::milliseconds(400));
+      try {
+        auto client_future = cf::async([client = std::move(client)] () mutable {
+          client.connect("mysite.com:8001");
+          return std::move(client);
+        }).timeout(std::chrono::milliseconds(500), connect_timeout("Connect timeout"), tw).then(executor,
+        [](cf::future<tcp_client> client_future) mutable {
+          auto client = client_future.get();
+          client.write("GET /");
+          return client;
+        }).timeout(std::chrono::milliseconds(500), write_timeout("Write timeout"), tw).then(executor,
+        [](cf::future<tcp_client> client_future) mutable {
+          auto client = client_future.get();
+          client.read_until("/r/n/r/n");
+          return client;
+        }).timeout(std::chrono::milliseconds(200), read_timeout("Read timeout"), tw);
+        
+        std::cout << client_future.get().data() << std::endl;
+        REQUIRE(false);
+
+      } catch (const connect_timeout& e) {
+        REQUIRE(false);
+        std::cerr << e.what() << std::endl;
+      } catch (const write_timeout& e) {
+        REQUIRE(false);
+      } catch (const read_timeout& e) {
+        REQUIRE(e.what() == std::string("Read timeout"));
+      }
+    }
+    
+    SECTION("no timeout") {
+      tcp_client client(std::chrono::milliseconds(100),
+                        std::chrono::milliseconds(100),
+                        std::chrono::milliseconds(400));
+      try {
+        auto client_future = cf::async([client = std::move(client)] () mutable {
+          client.connect("mysite.com:8001");
+          return std::move(client);
+        }).timeout(std::chrono::milliseconds(500), connect_timeout("Connect timeout"), tw).then(executor,
+        [](cf::future<tcp_client> client_future) mutable {
+          auto client = client_future.get();
+          client.write("GET /");
+          return client;
+        }).timeout(std::chrono::milliseconds(500), write_timeout("Write timeout"), tw).then(executor,
+        [](cf::future<tcp_client> client_future) mutable {
+          auto client = client_future.get();
+          client.read_until("/r/n/r/n");
+          return client;
+        }).timeout(std::chrono::milliseconds(700), read_timeout("Read timeout"), tw);
+        
+        auto client_data = client_future.get().data();
+        REQUIRE(client_data == "Some data");
+        std::cout << client_data << std::endl;
+
+      } catch (const connect_timeout& e) {
+        REQUIRE(false);
+        std::cerr << e.what() << std::endl;
+      } catch (const write_timeout& e) {
+        REQUIRE(false);
+      } catch (const read_timeout& e) {
+        REQUIRE(false);
       }
     }
   }
