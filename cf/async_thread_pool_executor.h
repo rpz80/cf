985b2a5090ab65ef2036918b3d938bd21e83e92a
rpz80 <rippa80@gmail.com>
2017-02-25 15:34:58 +0300

    Resolved race in tp executor.

--- cf/async_thread_pool_executor.h
+++ cf/async_thread_pool_executor.h
@@ -28,7 +28,7 @@ class async_thread_pool_executor {
   private:
     std::thread thread_;
     mutable std::mutex m_;
-    std::atomic<bool> need_stop_ = {false};
+    bool need_stop_ = {false};
     std::atomic<bool> has_task_ = {false};
     std::condition_variable start_cond_;
     detail::task_type task_;
