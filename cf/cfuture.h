503d85c122551337207823e2d1a51fec44f1a706
Maksim Golov <maksim.golov@leica-geosystems.com>
2018-11-09 14:18:02 +0100

    Add <string> header

--- cf/cfuture.h
+++ cf/cfuture.h
@@ -12,6 +12,7 @@
 #include <vector>
 #include <iterator>
 #include <tuple>
+#include <string>
 #include "common.h"
 
 #if !defined(__clang__) && defined (__GNUC__) && (__GNUC__ == 4 && __GNUC_MINOR__ <= 8)
